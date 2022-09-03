88e5db40a96aa7f79e22e88b5c299aeb291cf911
Salman Shaikh <attitude007780@gmail.com>
2022-09-03 21:40:57 +0530

    1 Sept

---
+++ arithmetic.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 20;
+
+c = a + b;
+d = a - b;
+e = a * b;
+f = a / b;
+console.log("Total of a plus b="+c);
+console.log("Total of a minus b="+d);
+console.log("Total of a multiply b="+e);
+console.log("Total of a divide b="+f);
