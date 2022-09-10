0b8e8e95da1d430caef1f24bf777e131b9f9ce42
Sanket <wearedevelopers19@gmail.com>
2022-09-10 16:34:57 +0530

    9_10_22

--- object_anonymous_function.js
+++ object_anonymous_function.js
@@ -1,46 +1,46 @@
 // for creating a object in javascript
 
-// var users = {name:"Salman Shaikh",age:27,email:"salmanshaikh007780@gmail.com"}
+var users = {name:"Salman Shaikh",age:27,email:"salmanshaikh007780@gmail.com"}
 
 // print all the objects
-// console.log(users);
+console.log(users);
 
 // print only select keys
-// console.log(users.name);
+console.log(users.name);
 
 // for creating anonymous functions
 
-// var person = function(){
-//     console.log("this an anonymous function")
-// }
+var person = function(){
+    console.log("this an anonymous function")
+}
 
-// person()
+person()
 
-// var person = function(a,b)
-// {
-//     var c = a + b;
-//     console.log("Total of a + b:"+c);
-// }
+var person = function(a,b)
+{
+    var c = a + b;
+    console.log("Total of a + b:"+c);
+}
 
-// person(10,20);
+person(10,20);
 
 // arrow function
 
-// var users = function(){
-//     console.log("this an arrow function")
-// }
+var users = function(){
+    console.log("this an arrow function")
+}
 
-// users()
+users()
 
-// var users =()=>console.log("this an arrow function")
-
-
-// users()
-
-// var users =(a)=>console.log("this an arrow function:"+a)
+var users =()=>console.log("this an arrow function")
 
 
-// users(5)
+users()
+
+var users =(a)=>console.log("this an arrow function:"+a)
+
+
+users(5)
 
 var users =(a,b)=>{
     var c = a + b; 
